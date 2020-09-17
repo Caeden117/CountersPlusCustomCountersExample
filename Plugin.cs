1157bb46b9e50ea4f456f77228bfbc3e61c8e584
Caeden117 <caeden.s@outlook.com>
2020-09-16 17:13:42 -0700

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,17 @@
+﻿using IPA;
+
+namespace CountersPlusNewCustomCountersTest
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        [Init]
+        public Plugin() { }
+
+        [OnStart]
+        public void OnApplicationStart() { }
+
+        [OnExit]
+        public void OnApplicationQuit() { }
+    }
+}
---
+++ WilliamGayCounter.cs
@@ -0,0 +1,30 @@
+﻿using CountersPlus.Counters.Custom;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace CountersPlusNewCustomCountersTest
+{
+    class WilliamGayCounter : BasicCustomCounter, ITickable
+    {
+        private TMP_Text williamGayness;
+        private ulong williamGay;
+
+        public override void CounterInit()
+        {
+            var header = CanvasUtility.CreateTextFromSettings(Settings);
+            header.text = "William Gay";
+            header.fontSize = 3;
+
+            williamGayness = CanvasUtility.CreateTextFromSettings(Settings, new Vector3(0, -0.4f, 0));
+        }
+
+        public override void CounterDestroy() { }
+
+        public void Tick()
+        {
+            williamGay++;
+            williamGayness.text = $"{williamGay}";
+        }
+    }
+}
